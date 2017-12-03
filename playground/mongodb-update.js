36b5110295ab8e16a728c5b939320715c9c4810f
Malush <malusev.ivan@gmail.com>
2017-12-03 14:00:29 +0100

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,37 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5a22d9f11ee6d34a1a9c2bf3')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5a22ce380478b2180a0d40a7')
+  }, {
+    $set: {
+      name: 'Ivan Malusev'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+  //db.close();
+});
