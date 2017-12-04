89306d92b1240329f56ff1430ccbf1f124a42834
Malush <malusev.ivan@gmail.com>
2017-12-04 22:39:09 +0100

    Add queries playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,42 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var id = '5a2401922d5dd51a81e9cfc6';
+
+if(!ObjectID.isValid(id)) {
+  console.log('ID not valid');
+}
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todos) => {
+// console.log('Todos', todos);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if(!todo){
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo by id', todo);
+// }).catch((e) => console.log(e));
+
+//user.findbyid
+
+User.findById('5a2401922d5dd51a81e9cfc6').then((user) => {
+  if(!user) {
+    return console.log('Unable to find user');
+  }
+
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+});
