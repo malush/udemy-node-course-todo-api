1a8afe9a37f991257ee5aaafa1d2ad8fdc82accb
Malush <malusev.ivan@gmail.com>
2018-01-10 22:38:35 +0100

    Add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -107,6 +107,7 @@ describe('DELETE /todos/:id', () => {
           return done(err);
         }
 
+
         Todo.findById(hexId).then((todos) => {
           expect(todos).toBeNull();
           done();
@@ -260,5 +261,56 @@ describe('POST /users', () => {
       })
       .expect(400)
       .end(done);
+  });
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toBeDefined();
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0].access).toBe('auth');
+          expect(user.tokens[0].token).toBe(res.headers['x-auth']);
+          // expect(user.tokens[0]).toInclude({
+          //   acceess: 'auth',
+          //   token: res.headers['x-auth']
+          // });
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: 'abc123'
+      })
+      .expect(404)
+      .expect((res) => {
+        expect(res.headers['x-auth']).not.toBeDefined();
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
   });
 });
